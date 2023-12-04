cf7b8ab048ac916ef0da4e7ba4494650325e81fa
felipeCadena <felipecaden@gmail.com>
2023-12-04 16:35:10 -0300

    req 2

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+  'User',
+  { 
+    id: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    displayName: DataTypes.STRING,
+    email: { 
+    type: DataTypes.STRING,
+    unique: true
+    },
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,
+      { foreignKey: 'user_id', as: 'blog_posts' });
+  };
+
+  return User;
+};
